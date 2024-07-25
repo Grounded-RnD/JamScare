ef5cfd9ca843ad8ab0597b67e3495f17d57956b0
Haza Nasrullah <hazanasrullah@gmail.com>
2024-07-25 07:24:43 +0700

    init

--- android/app/src/main/kotlin/com/example/jumpscare/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.jumpscare
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/id/grounded/jamscare/jamscare/MainActivity.kt
@@ -0,0 +1,6 @@
+package id.grounded.jamscare.jamscare
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
